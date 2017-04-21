69a5e2b55e86a95c8f6356f3b3b06cbe34afff84
Dmitry Rusakov <jkikksa@gmail.com>
2017-04-21 15:49:47 +0300

    fix make-drag

--- js/make-draggable.js
+++ js/make-draggable.js
@@ -1,52 +1,57 @@
 'use strict';
 
 window.makeDraggable = (function () {
+  var _element;
+  var startCoords = {};
+  var shift = {};
+  var _callback;
+
+  var onMouseMove = function (evt) {
+    evt.preventDefault();
+
+    shift = {
+      x: startCoords.x - evt.clientX,
+      y: startCoords.y - evt.clientY
+    };
+
+    startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    _element.style.top = (_element.offsetTop - shift.y) + 'px';
+    _element.style.left = (_element.offsetLeft - shift.x) + 'px';
+  };
+
+  var onMouseUp = function (evt) {
+    evt.preventDefault();
+
+    _callback(_element);
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
 
   /**
    * Makes element draggable.
    * @param {Element} handle The handle for which the element is dragged
-   * @param {Element} element What is dragged?
+   * @param {Element} element What is dragged
    * @param {Function} callback
    */
   return function (handle, element, callback) {
+    _element = element;
+    _callback = callback;
 
     handle.addEventListener('mousedown', function (evt) {
       evt.preventDefault();
 
-      var startCoords = {
+      startCoords = {
         x: evt.clientX,
         y: evt.clientY
-      };
-
-      var onMouseMove = function (moveEvt) {
-        moveEvt.preventDefault();
-
-        callback(element);
-
-        var shift = {
-          x: startCoords.x - moveEvt.clientX,
-          y: startCoords.y - moveEvt.clientY
-        };
-
-        startCoords = {
-          x: moveEvt.clientX,
-          y: moveEvt.clientY
-        };
-
-        element.style.top = (element.offsetTop - shift.y) + 'px';
-        element.style.left = (element.offsetLeft - shift.x) + 'px';
-      };
-
-      var onMouseUp = function (upEvt) {
-        upEvt.preventDefault();
-
-        document.removeEventListener('mousemove', onMouseMove);
-        document.removeEventListener('mouseup', onMouseUp);
       };
 
       document.addEventListener('mousemove', onMouseMove);
       document.addEventListener('mouseup', onMouseUp);
     });
-
   };
 })();
